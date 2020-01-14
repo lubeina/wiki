89536ba01c2bdf8cf275040ed3a5996046a8ebc2
lubeina <52536093+lubeina@users.noreply.github.com>
2020-01-14 18:53:10 +0300

    Changes Made

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Page(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    last_update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/detail/%i/" % self.id
+
 # Create your models here.
